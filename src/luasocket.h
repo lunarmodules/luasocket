b40d2ba005034c36c6b7f4439e6d6df1ef08d5e4
Diego Nehab <diego@tecgraf.puc-rio.br>
2001-01-13 07:11:44 +0000

    Included LUASOCKET_BUFFERSIZE and LUASOCKET_VERSION defines.

--- src/luasocket.h
+++ src/luasocket.h
@@ -7,12 +7,9 @@
 #ifndef _LUASOCKET_H_
 #define _LUASOCKET_H_
 
-/*=========================================================================*\
-* Exported function declarations
-\*=========================================================================*/
-/*-------------------------------------------------------------------------*\
-* Initializes toolkit
-\*-------------------------------------------------------------------------*/
+#define LUASOCKET_VERSION "LuaSocket 1.2"
+#define LUASOCKET_BUFFERSIZE 8192
+
 void lua_socketlibopen(lua_State *L);
 
 #endif /* _LUASOCKET_H_ */
