297576affae2113dce05970591adab3e75be65c0
Diego Nehab <diego@tecgraf.puc-rio.br>
2001-03-06 19:01:44 +0000

    Updated for release 1.2.1 Buffer size constants are now part of luasocket.h.

--- src/luasocket.h
+++ src/luasocket.h
@@ -7,9 +7,23 @@
 #ifndef _LUASOCKET_H_
 #define _LUASOCKET_H_
 
-#define LUASOCKET_VERSION "LuaSocket 1.2"
-#define LUASOCKET_BUFFERSIZE 8192
+/* Current luasocket version */
+#define LUASOCKET_VERSION "LuaSocket 1.2.1"
 
+/*-------------------------------------------------------------------------*\
+* These can be changed to according to the applications' needs.
+\*-------------------------------------------------------------------------*/
+/* TCP input buffer size */
+#define LUASOCKET_TCPBUFFERSIZE 8192
+
+/* The largest datagram handled by LuaSocket */
+#define LUASOCKET_UDPBUFFERSIZE 4096
+/* note that 576 bytes is the maximum safe value */
+
+/*-------------------------------------------------------------------------*\
+* Initializes the library interface with Lua and the socket library.
+* Defines the symbols exported to Lua.
+\*-------------------------------------------------------------------------*/
 void lua_socketlibopen(lua_State *L);
 
 #endif /* _LUASOCKET_H_ */
