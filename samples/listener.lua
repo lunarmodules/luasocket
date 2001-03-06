22a5d3f669c2ad57b422b4cfa8e06cb4713aa12f
Diego Nehab <diego@tecgraf.puc-rio.br>
2001-03-06 19:54:57 +0000

    Uptated for LuaSocket 1.2. The version released was incompatible...

--- samples/listener.lua
+++ samples/listener.lua
@@ -1,15 +1,16 @@
 host = host or "localhost"
-port = host or 8080
+port = port or 8080
 if arg then
 	host = arg[1] or host
 	port = arg[2] or port
 end
 print("Binding to host '" ..host.. "' and port " ..port.. "...")
-s, i, p, e = bind(host, port)
+s, e = bind(host, port)
 if not s then
 	print(e)
 	exit()
 end
+i, p = s:getsockname()
 print("Waiting connection from talker on " .. i .. ":" .. p .. "...")
 c, e = s:accept()
 if not c then
