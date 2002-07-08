c27087ca95e15ede5a2dcc0b160341fb9bed04ca
Diego Nehab <diego@tecgraf.puc-rio.br>
2002-07-08 20:44:12 +0000

    LuaSocket 1.5 (4.1w3)

--- src/luasocket.h
+++ src/luasocket.h
@@ -1,24 +1,17 @@
 /*=========================================================================*\
-* TCP/IP support for LUA
+* Networking support for the Lua language
 * Diego Nehab
 * 9/11/1999
+*
+* RCS ID: $Id$
 \*=========================================================================*/
-
 #ifndef _LUASOCKET_H_
 #define _LUASOCKET_H_
 
-/* Current luasocket version */
-#define LUASOCKET_VERSION "LuaSocket 1.4"
-
 /*-------------------------------------------------------------------------*\
-* These can be changed to according to the applications' needs.
+* Current luasocket version
 \*-------------------------------------------------------------------------*/
-/* TCP input buffer size */
-#define LUASOCKET_TCPBUFFERSIZE 8192
-
-/* The largest datagram handled by LuaSocket */
-#define LUASOCKET_UDPBUFFERSIZE 4096
-/* note that 576 bytes is the maximum safe value */
+#define LUASOCKET_VERSION "LuaSocket 1.5"
 
 /*-------------------------------------------------------------------------*\
 * This macro prefixes all exported API functions
@@ -28,8 +21,7 @@
 #endif
 
 /*-------------------------------------------------------------------------*\
-* Initializes the library interface with Lua and the socket library.
-* Defines the symbols exported to Lua.
+* Initializes the library.
 \*-------------------------------------------------------------------------*/
 LUASOCKET_API int lua_socketlibopen(lua_State *L);
 
