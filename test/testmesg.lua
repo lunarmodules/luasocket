e70d404a101f985c709b5caddf55853ac48cb4a6
Diego Nehab <diego@tecgraf.puc-rio.br>
2004-05-30 23:30:00 +0000

    doesn't fail on windows.

--- test/testmesg.lua
+++ test/testmesg.lua
@@ -1,3 +1,6 @@
+require("smtp")
+require("mime")
+
 mesgt = {
     headers = {
        to = "D Burgess <db@werx4.com>",
@@ -10,12 +13,12 @@ mesgt = {
         },
         [2] = {
             headers = {
-                ["content-type"] = 'application/octet-stream; name="luasocket"',
-                ["content-disposition"] = 'attachment; filename="luasocket"',
+                ["content-type"] = 'application/octet-stream; name="testmesg.lua"',
+                ["content-disposition"] = 'attachment; filename="testmesg.lua"',
                 ["content-transfer-encoding"] = "BASE64"
             },
             body = ltn12.source.chain(
-                ltn12.source.file(io.open("luasocket", "rb")),
+                ltn12.source.file(io.open("testmesg.lua", "rb")),
                 ltn12.filter.chain(
                     mime.encode("base64"),
                     mime.wrap()
@@ -40,10 +43,6 @@ mesgt = {
         epilogue = "Done attaching stuff",
     }
 }
-
--- sink = ltn12.sink.file(io.stdout)
--- source = ltn12.source.chain(socket.smtp.message(mesgt), socket.smtp.stuff())
--- ltn12.pump.all(source, sink)
 
 print(socket.smtp.send {
     rcpt = "<diego@cs.princeton.edu>",
