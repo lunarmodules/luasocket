ebde49944b1ba06153fe31a1ad92aaebf6e053e2
Diego Nehab <diego@impa.br>
2013-05-30 17:59:28 +0800

    Unix domain tests still broken.

--- test/utestclnt.lua
+++ test/utestclnt.lua
@@ -1,7 +1,7 @@
-require"socket"
-local socket = require"socket.unix"
+local socket = require"socket"
+socket.unix = require"socket.unix"
 
-host = "luasocket"
+host = host or "luasocket"
 
 function pass(...)
     local s = string.format(...)
@@ -82,7 +82,7 @@ function check_timeout(tm, sl, elapsed, err, opp, mode, alldone)
     end
 end
 
-if not socket.DEBUG then
+if not socket._DEBUG then
     fail("Please define LUASOCKET_DEBUG and recompile LuaSocket")
 end
 
@@ -115,7 +115,7 @@ else pass("connected!") end
 
 ------------------------------------------------------------------------
 function test_methods(sock, methods)
-    for _, v in methods do
+    for _, v in pairs(methods) do
         if type(sock[v]) ~= "function" then 
             fail(sock.class .. " method '" .. v .. "' not registered") 
         end
--- test/utestsrvr.lua
+++ test/utestsrvr.lua
@@ -1,10 +1,10 @@
-require("socket");
+socket=require("socket");
 os.remove("/tmp/luasocket")
-socket = require("socket.unix");
-host = "luasocket";
-server = socket.unix()
-print(server:bind(host))
-print(server:listen(5))
+socket.unix = require("socket.unix");
+host = host or "luasocket";
+server = assert(socket.unix())
+assert(server:bind(host))
+assert(server:listen(5))
 ack = "\n";
 while 1 do
     print("server: waiting for client connection...");
@@ -12,6 +12,6 @@ while 1 do
     while 1 do 
         command = assert(control:receive());
         assert(control:send(ack));
-        (loadstring(command))();
+        ((loadstring or load)(command))();
     end
 end
