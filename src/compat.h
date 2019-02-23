59c8aaac3425108e3137677863cc8cee6c84e20b
Philipp Janda <siffiejoe@gmx.net>
2019-02-23 10:32:25 +0100

    Fix static linking problem with LuaJIT
    LuaJIT and LuaSocket both define new Lua APIs from Lua 5.2 (in
    particular `luaL_setfuncs` and `luaL_testudata`). When linking both
    statically, the one definition rule strikes and linking fails. This
    commit fixes the issue by renaming the LuaSocket versions of those
    functions behind the scenes using the C preprocessor.
    
    Closes #214

--- src/compat.h
+++ src/compat.h
@@ -5,6 +5,8 @@
 #include "lauxlib.h"
 
 #if LUA_VERSION_NUM==501
+#define luaL_setfuncs socket_setfuncs
+#define luaL_testudata socket_testudata
 void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup);
 void *luaL_testudata ( lua_State *L, int arg, const char *tname);
 #endif
