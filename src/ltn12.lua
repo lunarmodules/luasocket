46ecb7e2dc3dd92931c6d9d99fdc9260e83077ff
david <57832272+jyoui@users.noreply.github.com>
2022-02-01 15:40:35 +0900

    src/ltn12.lua: remove duplicated codes

--- src/ltn12.lua
+++ src/ltn12.lua
@@ -11,6 +11,8 @@ local string = require("string")
 local table = require("table")
 local unpack = unpack or table.unpack
 local base = _G
+local select = select
+
 local _M = {}
 if module then -- heuristic for exporting a global package table
     ltn12 = _M  -- luacheck: ignore
@@ -21,8 +23,6 @@ _M.filter = filter
 _M.source = source
 _M.sink = sink
 _M.pump = pump
-
-local unpack = unpack or table.unpack
 
 -- 2048 seems to be better in windows...
 _M.BLOCKSIZE = 2048
@@ -45,7 +45,7 @@ end
 -- (thanks to Wim Couwenberg)
 function filter.chain(...)
     local arg = {...}
-    local n = base.select('#',...)
+    local n = select('#',...)
     local top, index = 1, 1
     local retry = ""
     return function(chunk)
