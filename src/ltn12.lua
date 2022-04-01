a308c442a849726a30da6f1ac33f5607f29dc954
C3pa <41503714+C3pa@users.noreply.github.com>
2022-04-01 22:19:17 +0200

    chore: Annotate lowercase global to avoid EmmyLua warning
    Adds and EmmyLua annotation to skip diagnostics for the next line. Here is some more [info](https://github.com/sumneko/lua-language-server/wiki/EmmyLua-Annotations#diagnostic).

--- src/ltn12.lua
+++ src/ltn12.lua
@@ -15,6 +15,7 @@ local select = select
 
 local _M = {}
 if module then -- heuristic for exporting a global package table
+    ---@diagnostic disable-next-line:lowercase-global
     ltn12 = _M  -- luacheck: ignore
 end
 local filter,source,sink,pump = {},{},{},{}
