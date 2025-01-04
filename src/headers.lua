4160ef18350174395828381a6679a1a90b2d6567
johnd0e <1838643+johnd0e@users.noreply.github.com>
2025-01-04 17:07:39 +0100

    Proper case for set of predefined parts
    Ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers

--- src/headers.lua
+++ src/headers.lua
@@ -7,11 +7,37 @@ local socket = require("socket")
 socket.headers = {}
 local _M = socket.headers
 
+local parts = {
+  ch="CH",
+  dns="DNS",
+  ect="ECT",
+  etag="ETag",
+  gpc="GPC",
+  id="ID",
+  md5="MD5",
+  mime="MIME",
+  mta="MTA",
+  nel="NEL",
+  rtt="RTT",
+  smtp="SMTP",
+  sourcemap="SourceMap",
+  te="TE",
+  ua="UA",
+  websocket="WebSocket",
+  wow64="WoW64",
+  www="WWW",
+  xss="XSS",
+}
+
 _M.canonic = setmetatable({},{
   __index=function (t,k)
-    t[k] = string.gsub(k, "%f[%w]%l", string.upper)
+    t[k] = string.gsub(k, "%f[%w]%l", function (part)
+      return parts[part] or string.upper(part)
+    end)
     return t[k]
   end;
 })
+
+_M.parts = parts
 
 return _M
